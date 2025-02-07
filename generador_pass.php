b3caf9c508da64e7c7f8e9381a3a24c08ef4d048
Servicio tecnico cym <tecnicocym500@gmail.com>
2025-02-07 14:58:57 +0000

    mostrar variable max index

--- generador_pass.php
+++ generador_pass.php
@@ -6,6 +6,8 @@ function generar_password($longitud, $caracteres_permitidos){
     $password = '';
     $max_index = strlen($caracteres_permitidos)-1;
 
+    echo '<h1>Muestra la variable max_index</h1>' . $max_index; // Mostrar el valor de max_index
+    
     for ($i = 0; $i < $longitud; $i++){
         $password .= $caracteres_permitidos[random_int(0, $max_index)];
     }
